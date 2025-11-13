373c8241e595d8b816eff453f918522591cfa4eb
Namyejun <yejun1148@gmail.com>
2025-11-13 15:09:13 +0900

    refactor: 타입 수정

--- src/main/java/com/jumunhasyeotjo/userinteract/message/domain/repository/MessageRepository.java
+++ src/main/java/com/jumunhasyeotjo/userinteract/message/domain/repository/MessageRepository.java
@@ -5,8 +5,10 @@ import com.jumunhasyeotjo.userinteract.message.domain.vo.UserId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.UUID;
+
 public interface MessageRepository {
     Message save(Message message);
-    Message findById(Long id);
+    Message findById(UUID id);
     Page<Message> findAllByUserId(UserId userId, Pageable pageable);
 }
